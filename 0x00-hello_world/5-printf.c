7ebeba49d6035f76815449d4258b4db2762e9502
Ajamal95 <ahmedjamal95@gmail.com>
2023-09-16 10:47:39 -0700

    me

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entery pint
+ *
+ * Descripion: A C program that prints wit print function.
+ *
+ * Return: Always 0 (Sucess)
+*/
+
+int main(void)
+{
+	ptintf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
