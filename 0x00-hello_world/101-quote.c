96c118203c8c0d8b582117f1b42a97f00915f563
Ajamal95 <ahmedjamal95@gmail.com>
2023-09-16 11:55:31 -0700

    forgotthedot

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,13 +4,13 @@
  * main - Entery point
  *
  * Description: print a quote using write function
- * sszie_t write(int fd. const void *buf. size_t count);
+ *	sszie_t write(int fd. const void *buf. size_t count);
  *
- * Return 1 (error).
+ * Return: 1 (error).
 */
-int (void)
-{
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+int main(void)
+{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1, quo, 59);
 	return (1);
