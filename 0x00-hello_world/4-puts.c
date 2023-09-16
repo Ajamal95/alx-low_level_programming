db814a4e67b8bd90630d1345e7b560315a95b908
Ajamal95 <ahmedjamal95@gmail.com>
2023-09-16 10:31:17 -0700

    11

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main Entery point
+ * Discription: a Cprogram that prints with put function
+ * Return: Alwyas 0 (Succes)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
