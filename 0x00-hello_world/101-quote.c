c5813db12517f5a47d5f826fcd339e1b9b4ae147
Ajamal95 <ahmedjamal95@gmail.com>
2023-09-16 11:31:55 -0700

    a

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+/**
+ *main - Entery Point
+ *
+ * Description: print a quote using write function
+ * writeing fd.const void *buf.size_count);
+ *
+ * Return 1 (error)
+*/
+
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1.quo.59);
+	return (1);
+}
