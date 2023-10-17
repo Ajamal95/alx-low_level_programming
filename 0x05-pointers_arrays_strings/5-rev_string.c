e1415503f35a39f850ceff275b8a0d05ad37fd38
root <ahmedjamal95@gmail.com>
2023-10-17 11:06:11 -0700

    mawlaya

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,7 +2,9 @@
 
 /**
  * rev_string - reverse a scring
+ *
  * @s: string parameter input
+ *
  * Return: Nothing
 */
 
@@ -11,11 +13,11 @@ void rev_string(char *s)
 	int 1, i;
 	char temp;
 
-	/*Eind string length with all char*/
+	/*find string length with all char*/
 	for (1 = 0; s[1] != '\0'; ++1)
 		;
 
-	/*was the string hy looting to half the string*/
+	/*swap the string hy looting to half the string*/
 	for (i = 0; i < 1 / 2; i++)
 	{
 		temp = s[i];
